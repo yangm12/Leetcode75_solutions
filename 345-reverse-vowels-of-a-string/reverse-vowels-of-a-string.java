c8050811550130ddb27f6e754e734e07270dd315
Mandy Yang <78169579+yangm12@users.noreply.github.com>
2025-06-01 15:37:28 -0400

    Time: 10 ms (14.34%) | Memory: 45.1 MB (44.10%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -8,10 +8,14 @@ class Solution {
         // Find vowels in s and their corresponding positions
         // Reverse the vowels and insert back to their corresponding positions
         char[] chars = s.toCharArray();
+
+        // Get vowels' ASCII codes in an immutable list
         List<Integer> vowelAsciiCodes = List.of(97, 101, 105, 111, 117,
             65, 69, 73, 79, 85);
         ArrayList<Character> vowels = new ArrayList<>();
         ArrayList<Integer> pos = new ArrayList<>();
+
+        // Loop through each char in s and check if this is a vowel
         for(int i=0; i<chars.length; i++){
             if(vowelAsciiCodes.contains((int)chars[i])){
                 vowels.add(chars[i]);
@@ -19,13 +23,10 @@ class Solution {
             }
         }
         Collections.reverse(vowels);
+        // Place the reversed vowels back
         for(int i=0; i<vowels.size(); i++){
             chars[pos.get(i)] = vowels.get(i);
         }
-
-        System.out.println(chars);
-
-
 
         return new String(chars);
     }
