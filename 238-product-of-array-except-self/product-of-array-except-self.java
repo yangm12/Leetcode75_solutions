a2d89a931d18ca5cd2686cb87b345deea9e1d1dd
Mandy Yang <78169579+yangm12@users.noreply.github.com>
2025-06-05 15:52:03 -0400

    Time: 3 ms (22.40%) | Memory: 55.2 MB (94.22%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,19 +1,23 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] res = new int[nums.length];
+
         // Constraints checking
         if(nums.length<2||nums.length>100000){
             return res;
         }
 
+        // Initialising the result array with all 1s.
         Arrays.fill(res, 1); 
 
+        // Find the prefix product. 
         int temp=1;
         for (int i=0; i<nums.length; i++){
             res[i]*=temp;
             temp*=nums[i];
         }
 
+        // Find the suffix product. 
         temp = 1;
         for (int i=nums.length-1; i>=0; i--){
             res[i]*=temp;
