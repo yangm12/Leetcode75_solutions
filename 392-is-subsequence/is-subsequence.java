09d39fc9c08b71d391b58055a5632e0d6c496a9b
Mandy Yang <78169579+yangm12@users.noreply.github.com>
2025-06-08 23:45:09 -0400

    Time: 1 ms (93.45%) | Memory: 41.3 MB (86.15%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -8,20 +8,23 @@ class Solution {
         if(s.length()==0){
             return true;
         }
-
-        int pt = 0;
+        
+        int pt = 0;     // Points to the current char in s
         char[] str = s.toCharArray();
 
         for(char c:t.toCharArray()){
+            // The pointer is out of bound -> s is fully traversed
             if(pt>=str.length){
                 return true;
             }
-            
+
+            // If matches, increment pointer
             if(str[pt]==c){
                 pt++;
             }
         }
 
+        // Check whether s is fully traversed after the loop
         if(pt==str.length){
             return true;
         }else{
