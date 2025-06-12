443fcb7dec6672c68208a0b189948f311b1b4424
Mandy Yang <78169579+yangm12@users.noreply.github.com>
2025-06-12 16:01:08 -0400

    Time: 10 ms (76.64%) | Memory: 45.4 MB (75.06%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
@@ -8,26 +8,15 @@ class Solution {
         Set<Integer> set1 = new HashSet<>();
         Set<Integer> set2 = new HashSet<>();
 
-        for (int num : nums1) {
-            set1.add(num);
-        }
-        for (int num : nums2) {
-            set2.add(num);
-        }
+        for(int num: nums1) set1.add(num);
+        for(int num: nums2) set2.add(num);
 
         List<Integer> list1 = new ArrayList<>();
         List<Integer> list2 = new ArrayList<>();
 
-        for (int num : set1) {
-            if (!set2.contains(num)) {
-                list1.add(num);
-            }
-        }
-        for (int num : set2) {
-            if (!set1.contains(num)) {
-                list2.add(num);
-            }
-        }
+        for(int num: set1) if(!set2.contains(num)) list1.add(num);
+        for(int num: set2) if(!set1.contains(num)) list2.add(num);
+        
         return Arrays.asList(list1, list2);
     }
 }
