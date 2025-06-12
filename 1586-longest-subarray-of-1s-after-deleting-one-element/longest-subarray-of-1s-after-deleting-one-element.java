ae146a218329c253aa1fecea20dd7df08448eb43
Mandy Yang <78169579+yangm12@users.noreply.github.com>
2025-06-12 13:53:30 -0400

    Time: 3 ms (79.50%) | Memory: 56.8 MB (59.54%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
@@ -3,6 +3,7 @@ class Solution {
         // Constraints checking
         if(nums.length<1||nums.length>100000) return 0;
 
+        /* Beats 98.04% runtime, 6.59% memory
         int left = 0;
         int k = 1;
         for(int right=0; right<nums.length; right++){
@@ -17,5 +18,22 @@ class Solution {
             }
         }
         return nums.length-left-1;
+        */
+
+        int left = 0, zeros = 0, ans = 0;
+        for(int right=0; right<nums.length; right++){
+            if(nums[right]==0){
+                zeros++;
+            }
+            while(zeros>1){
+                if(nums[left]==0){
+                    zeros--;
+                }
+                left++;
+            }
+            ans = Math.max(ans, right-left+1-zeros);
+        }
+        return (ans==nums.length)? ans-1:ans;
+
     }
 }
