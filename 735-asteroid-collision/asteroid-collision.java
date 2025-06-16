9584c492e8219a9c01a3d340907eb991aab9638b
Mandy Yang <78169579+yangm12@users.noreply.github.com>
2025-06-15 22:17:51 -0400

    Time: 5 ms (84.51%) | Memory: 45.8 MB (11.87%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -3,6 +3,7 @@ class Solution {
         // Constraints checking
         if(asteroids.length<2||asteroids.length>10000) return new int[0]; 
 
+        /* Using stack, beats 64.93% in runtime, 11.87% in memory
         Stack<Integer> as = new Stack<>();
         for(int a:asteroids){
             if(a>0){
@@ -21,5 +22,30 @@ class Solution {
             res[i--]=as.pop();
         }
         return res;
+        */
+
+        List<Integer> list = new ArrayList<>();
+
+        for(int i=0; i<asteroids.length; i++){
+            if(asteroids[i]>0){
+                list.add(asteroids[i]);
+            }else{
+                while(list.size()!=0 && list.get(list.size()-1)>0 && list.get(list.size()-1)<Math.abs(asteroids[i])){
+                    list.remove(list.size()-1);
+                }
+                if(list.size()==0 || list.get(list.size()-1)<0){
+                    list.add(asteroids[i]); 
+                }
+                if(list.get(list.size()-1)==Math.abs(asteroids[i])){
+                    list.remove(list.size()-1);
+                }
+            }
+        }
+
+        int[] arr = new int[list.size()];
+        for (int i=0; i<list.size(); i++) {
+            arr[i] = list.get(i);
+        }
+        return arr;
     }
 }
