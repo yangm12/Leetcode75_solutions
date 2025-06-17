7591ee5e667847c0d259eea1c0d035c7da55a041
Mandy Yang <78169579+yangm12@users.noreply.github.com>
2025-06-17 18:26:27 -0400

    Time: 10 ms (87.99%) | Memory: 45.9 MB (39.38%) - LeetSync

--- 649-dota2-senate/dota2-senate.java
+++ 649-dota2-senate/dota2-senate.java
@@ -4,8 +4,8 @@ class Solution {
         if(senate.length()<1||senate.length()>10000) return "";
 
         char[] chars = senate.toCharArray();
-        Queue<Integer> rs = new ArrayDeque<>();
-        Queue<Integer> ds = new ArrayDeque<>();
+        Queue<Integer> rs = new LinkedList<>();
+        Queue<Integer> ds = new LinkedList<>();
         int n = senate.length();
         for(int i=0; i<n; i++){
             if(chars[i]=='R'){
@@ -17,12 +17,11 @@ class Solution {
             }
             
         }
-        System.out.println(rs);
 
         while(!rs.isEmpty() && !ds.isEmpty()){
-            int r = rs.poll();
-            int d = ds.poll();
-            if(r<d){
+            // int r = rs.poll();
+            // int d = ds.poll();
+            if(rs.poll()<ds.poll()){
                 rs.offer(n);
             }else{
                 ds.offer(n);
