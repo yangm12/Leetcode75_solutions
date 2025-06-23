b1feeb5d20fbaefd9a7300fd3822514c79a8ef29
Mandy Yang <78169579+yangm12@users.noreply.github.com>
2025-06-22 23:07:06 -0400

    Time: 4 ms (97.95%) | Memory: 63.4 MB (85.32%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
@@ -12,6 +12,7 @@ class Solution {
     public int pairSum(ListNode head) {
         if(head.next==null) return head.val+head.next.val;
         
+        /* Beats 29.86% runtime, 32.08% memory
         List<Integer> list = new ArrayList<>();
         ListNode temp = head;
         while(temp.next!=null){
@@ -29,6 +30,33 @@ class Solution {
             right--;
         }
         return res;
-        
+        */
+
+        // 1. Find the middle index using slow-fast pointers
+        ListNode slow = head, fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // 2. Reverse the second half of the list
+        ListNode prev = null;
+        while(slow!=null){
+            ListNode nextNode = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = nextNode;
+        }
+
+        // 3. Find twin sums from the start and the reversed second half
+        int res = 0;
+        ListNode start = head;
+        ListNode end = prev;    // The last node of the reversed second half
+        while(end!=null){
+            res = Math.max(res, start.val+end.val);
+            start = start.next;
+            end = end.next;
+        }
+        return res;
     }
 }
