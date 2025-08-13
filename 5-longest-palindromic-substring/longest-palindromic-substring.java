1c9b9cd9e4d63a047932937d683ef3855e44d65b
Mandy Yang <78169579+yangm12@users.noreply.github.com>
2025-08-13 11:18:22 -0400

    Time: 6 ms (99.31%) | Memory: 43 MB (53.33%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,5 +1,5 @@
 class Solution {
-    // 2 pointers
+    /* Approach 1: expand from centre
     public String longestPalindrome(String s) {
         if(s.length()<=1) return s;
 
@@ -33,5 +33,66 @@ class Solution {
 
         // Left pointer may go beyond to -1
         return s.substring(left+1,right);
+    }
+    */
+
+    // Approach 2: Manacher Algorithm
+    public String longestPalindrome(String s) {
+        if(s.length()<=1) return s;
+
+        // preprocecss: ^#a#b#b#a#$ (add ^ and $ to the start and the end of the str, use # to separate each char)
+        char[] treated = preprocess(s);
+        int n = treated.length;
+        int[] p = new int[n];   // stores radius for each centre i
+
+        // Current centre and right boundary
+        int centre = 0, right = 0;
+
+        for(int i=1; i<n-1; i++){
+            int mirror = 2*centre - i;
+
+            if(i<right){
+                p[i] = Math.min(right-i, p[mirror]);
+            }else{
+                p[i] = 0;
+            }
+
+
+            // Expanding around i
+            while(treated[i+1+p[i]] == treated[i-1-p[i]]){
+                p[i]++;
+            }
+
+
+            if(i+p[i]>right){
+                centre = i;
+                right = i+p[i];
+            }
+        }
+
+        int maxLen = 0;
+        int cidx=0;
+        for(int i=1; i<n-1; i++){
+            if(p[i]>maxLen){
+                maxLen=p[i];
+                cidx = i;
+            }
+        }
+
+        int start = (cidx-maxLen)/2;
+        return s.substring(start, start+maxLen);
+    }
+
+    private char[] preprocess(String s) {
+        int m = s.length();
+        char[] t = new char[2 * m + 3];
+        t[0] = '^';
+        for (int i = 0; i < m; i++) {
+            t[2 * i + 1] = '#';
+            t[2 * i + 2] = s.charAt(i);
+        }
+        t[2 * m + 1] = '#';
+        t[2 * m + 2] = '$';
+        return t;
     }
 }
