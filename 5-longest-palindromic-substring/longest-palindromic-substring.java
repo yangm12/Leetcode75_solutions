60c6e1e115eb5a0c20b5a833f350c4cde27e7fd9
Mandy Yang <78169579+yangm12@users.noreply.github.com>
2025-08-13 10:15:28 -0400

    Time: 19 ms (51.68%) | Memory: 45.6 MB (18.25%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -0,0 +1,37 @@
+class Solution {
+    // 2 pointers
+    public String longestPalindrome(String s) {
+        if(s.length()<=1) return s;
+
+        String maxStr = s.substring(0, 1);
+
+        for(int i=0; i<s.length()-1; i++){
+
+            // Mirror: curr char
+            String odd = expandFromCenter(s, i, i);
+
+            // Mirror: curr and the next char
+            String even = expandFromCenter(s, i, i+1);
+
+            if(odd.length() > maxStr.length()){
+                maxStr = odd;
+            }
+            if(even.length() > maxStr.length()){
+                maxStr = even;
+            }
+        }
+
+        return maxStr;
+    }
+
+    private String expandFromCenter(String s, int left, int right){
+        // Boundary control and compare char at left and right position
+        while(left>=0 && right<s.length() && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
+        }
+
+        // Left pointer may go beyond to -1
+        return s.substring(left+1,right);
+    }
+}
