7c22268c6945fa9287812b5fd1a950a4c195e526
Mandy Yang <78169579+yangm12@users.noreply.github.com>
2025-09-16 21:58:42 -0400

    Time: 0 ms (100.00%) | Memory: 40.4 MB (66.67%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -3,6 +3,7 @@ class Solution {
     public int climbStairs(int n) {
         if(n==1 || n==0) return 1;
 
+        /*
         int[] dp = new int[n+1];
         dp[0] = 1;
         dp[1] = 1;
@@ -10,7 +11,17 @@ class Solution {
         for(int i=2;i<=n;i++){
             dp[i] = dp[i-1] + dp[i-2];
         }
-        
+
         return dp[n];
+        */
+
+        // Space optim
+        int prev = 1, curr = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = curr;
+            curr = prev + curr;
+            prev = temp;
+        }
+        return curr;
     }
 }
