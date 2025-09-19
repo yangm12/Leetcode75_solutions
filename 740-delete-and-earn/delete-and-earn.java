744c1668e06c3857056b7ee49e5e371ddabbd896
Mandy Yang <78169579+yangm12@users.noreply.github.com>
2025-09-19 14:22:53 -0400

    Time: 2 ms (98.14%) | Memory: 44.6 MB (56.09%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.java
+++ 740-delete-and-earn/delete-and-earn.java
@@ -1,13 +1,18 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
-        int n = 100001;
-        int[] values = new int[n];
+        int n = 0;
+        for(int num: nums){
+            n = Math.max(num, n);
+        }
+
+        int[] values = new int[n+1];
         for(int num:nums){
             values[num] += num;
         }
+
         int takei = values[0];
         int skipi = 0;
-        for(int i=1;i<n;i++){
+        for(int i=1;i<=n;i++){
             int temp = takei;
             takei = skipi + values[i];
             skipi = Math.max(skipi, temp);
