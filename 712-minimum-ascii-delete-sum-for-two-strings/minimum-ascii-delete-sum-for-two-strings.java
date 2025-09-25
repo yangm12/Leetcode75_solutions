3c828a7af779cad57288b4b2ebfd69d09bc50f42
Mandy Yang <78169579+yangm12@users.noreply.github.com>
2025-09-24 21:17:26 -0400

    Time: 36 ms (5.07%) | Memory: 44.7 MB (87.89%) - LeetSync

---
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -0,0 +1,33 @@
+class Solution {
+    static Map<Character, Integer> dict = new HashMap<>();
+    public int minimumDeleteSum(String s1, String s2) {
+        int[][] dp = new int[s1.length()+1][s2.length()+1];
+        for(int i=0;i<=s1.length();i++){
+            for(int j=0;j<=s2.length();j++){
+                dp[i][j] = -1;
+            }
+        }
+        dp[0][0] = 0;
+
+        return solution(dp, s1, s2, s1.length(), s2.length());
+    }
+
+    private int solution(int[][] dp, String s1, String s2, int i, int j){
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(i==0){
+            return solution(dp, s1, s2, 0, j-1) + s2.charAt(j-1);
+        }
+        if(j==0){
+            return solution(dp, s1, s2, i-1, 0) + s1.charAt(i-1);
+        }
+        
+        if(s1.charAt(i-1) == s2.charAt(j-1)){
+            dp[i][j] = solution(dp, s1, s2, i-1, j-1);
+        }else{
+            dp[i][j] = Math.min(solution(dp, s1, s2, i-1, j)+s1.charAt(i-1),
+                                solution(dp, s1, s2, i, j-1)+s2.charAt(j-1)
+            );
+        }
+        return dp[i][j];
+    }
+}
